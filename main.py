20c01f22788b63c1f61b56c7e394cce9a82f2452
compiledchaos <sachinprathik8@gmail.com>
2025-08-14 11:52:20 +0530

    cleaned_up_ files

--- main.py
+++
@@ -1,9 +0,0 @@
-from api_watchdog.cli import parse_args
-from api_watchdog.entry import run_cli, run_gui
-
-if __name__ == "__main__":
-    args = parse_args()
-    if args.cli:
-        run_cli()
-    else:
-        run_gui()
